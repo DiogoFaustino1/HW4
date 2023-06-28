3ba4b18273f40c0dd7269f07fa0f394b7541a685
fvalquaresma <filipevalquaresma@tecnico.ulisboa.pt>
2023-06-28 15:03:23 +0100

    mesh convergence funciona bem creio eu
    siuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- mesh_convergence.py
+++ mesh_convergence.py
@@ -23,7 +23,10 @@ import time
 num_x_array = [2, 3, 5, 7, 9, 11]
 num_y_array = [2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25]
 
-# Initialize arrays to store results
+
+## Mesh Convergence Analysis for changes in # of chordwise points
+
+# Initialize arrays to store results for num_x variations
 CDx = np.zeros(len(num_x_array))              # CD array
 CDx_deltas = np.zeros(len(num_x_array)-1) 
 WBMx = np.zeros(len(num_x_array))             # Wingbox Mass array
@@ -31,8 +34,6 @@ WBMx_deltas = np.zeros(len(num_x_array)-1)
 Tx = np.zeros(len(num_x_array))               # CPU times array
 Tx_deltas = np.zeros(len(num_x_array)-1)
 
-
-## Mesh Convergence Analysis for changes in # of chordwise points
 # Iterates different num_x and stores values in array
 for i in range(len(num_x_array)):
     start = time.time()
@@ -56,10 +57,34 @@ print("CPU time value differences vs. num_x:", Tx_deltas)
 
 ## Mesh Convergence Analysis for changes in # of spanwise points
 
-# acabo amanhã lol vou dormir
+# Initialize arrays to store results for num_y variations
+CDy = np.zeros(len(num_y_array))              # CD array
+CDy_deltas = np.zeros(len(num_y_array)-1) 
+WBMy = np.zeros(len(num_y_array))             # Wingbox Mass array
+WBMy_deltas = np.zeros(len(num_y_array)-1)
+Ty = np.zeros(len(num_y_array))               # CPU times array
+Ty_deltas = np.zeros(len(num_y_array)-1)
 
 
-
+# Iterates different num_y and stores values in array
+for i in range(len(num_y_array)):
+    start = time.time()
+    CDy[i], WBMy[i] = MDA_mesh(5, num_y_array[i])
+    end = time.time()
+    Tx[i] = end-start
+    
+# Iterates through obtained values, determines deltas (in %), and stores in array
+for i in range(len(num_y_array) - 1):
+    CDy_deltas[i] = ((CDy[i + 1] - CDy[i]) / CDy[i]) * 100
+    WBMy_deltas[i] = ((WBMy[i + 1] - WBMy[i]) / WBMy[i]) * 100
+    Ty_deltas[i] = ((Ty[i + 1] - Ty[i]) / Ty[i]) * 100
+    
+print("CD values vs. num_y:", CDy)
+print("CD value differences vs. num_y:", CDy_deltas)
+print("Wingbox mass values vs. num_y:", WBMy)
+print("Wingbox mass value differences vs. num_y:", WBMy_deltas)
+print("CPU time values vs. num_y:", Ty)
+print("CPU time value differences vs. num_y:", Ty_deltas)
 
 # fig, ax = plt.subplots()
 # ax.plot(num_y_values, CPU_times, marker='o')
